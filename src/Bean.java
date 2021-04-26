1c5207fb5527c7b28d8965371f5221e9fe493295
Jeanne Kazakova <johndoe@example.comjeannekazakova@gmail.com>
2021-04-26 20:40:03 +0300

    111t

---
+++ src/Bean.java
@@ -0,0 +1,17 @@
+public class Bean {
+    private int value1;
+
+    public Bean(int value1) {
+        this.value1=value1;
+    }
+
+    public int getValue1
+
+    {
+        return value1;
+    }
+
+    public void setValue1(int getValue1) {
+        this.value1 =value1;
+    }
+}
